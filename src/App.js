5d3e080f5f6ae597b61ead986f5c500474f0cba0
GB-Navarro <gbnavarro@usp.br>
2022-08-08 23:11:12 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import GlobalStyle from "./assets/styles/globalStyle.js"
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle/>
+      <h1>Hello World!</h1>
+    </>  
+  );
+}
+
