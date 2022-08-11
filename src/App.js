88a497785f703b325ad73996c86f8e5c6f043bdf
Guilherme Silva <102394141+guilhermegssilva@users.noreply.github.com>
2022-08-11 18:33:45 -0300

    adicionada rota com parametro hashtag

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyle from "./assets/styles/globalStyle.js"
 import Timeline from "./components/Timeline/Timeline.js";
+import HashtagPage from '../components/HashtagPage/HashtagPage.jsx';
+
 export default function App() {
+  const [update, setUpdate] = useState(false)
+  
+  const HashtagHolder = () => {
+    const {hashtag} = useParams()
+    return <HashtagPage key={hashtag} updateState={[update, setUpdate]}/>
+  }
+    
   return (
     <>
       <GlobalStyle/>
       <BrowserRouter>
         <Routes>
           <Route path="/timeline" element={<Timeline />}></Route>
+          <Route path='/hashtag/:hashtag' element={ <HashtagHolder /> } />
         </Routes>
       </BrowserRouter>
       
