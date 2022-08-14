2e5e4f8c7730c75386db9aa7e74421236d14180e
Guilherme Silva <102394141+guilhermegssilva@users.noreply.github.com>
2022-08-14 15:38:26 -0300

    Add files via upload

---
+++ src/components/HashtagPage/HashtagPage.jsx
@@ -0,0 +1,34 @@
+import {useEffect, useState, useContext} from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+import UserContext from "../../../contexts/UserContext";
+import TimeLine from "../../TimeLine/TimeLine";
+
+
+export default function HashtagPage({ updateState }){
+    const [postsList, setPostsList] = useState([]);
+    const [userState] = useContext(UserContext);
+    const {hashtag} = useParams();
+    const [currentPage, setCurrentPage] = useState('2040-09-28T22:59:02.448804522Z');
+
+    useEffect(() => {
+        const config = {headers: { authorization: `Bearer ${userState.token}`}};
+        const URL = process.env.REACT_APP_API_URL+`/hashtag/${hashtag}?timestamp=` + currentPage;
+        const promise = axios.get(URL, config);
+        promise.then(response => {
+            setPostsList(previousPosts => [...previousPosts, ...response.data]);
+        });
+        promise.catch(error => console.log(error));
+    }, [currentPage]);
+
+    return (<TimeLine
+        updateState={updateState}
+        title={"# " + hashtag}
+        postsList={postsList}
+        setPostsList={setPostsList}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        timeline={false}
+    />);
+}
