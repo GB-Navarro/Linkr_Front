1139aa94cd23ef913c8c8eb1b2a48b73ea0f6f82
GB-Navarro <gbnavarro@usp.br>
2022-08-14 17:29:47 -0300

    refactor: the code of Timeline component has been refactored and DeleteModal component has been created

---
+++ src/components/Timeline/DeleteModal/DeleteModal.js
@@ -0,0 +1,26 @@
+import Modal from "react-modal";
+import customStyles from "../functions/modalStyles.js";
+import { afterOpenModal, closeModal } from "../functions/modalFunctions.js";
+import {DeleteText, DeleteTextBox, Button, ButtonBox} from "./styles.js";
+import { useState } from "react";
+export default function DeleteModal(props){
+
+    let subtitle;
+    
+    Modal.setAppElement('#root');
+
+    return(
+        <>
+            <Modal isOpen={props.modalIsOpen} onAfterOpen={() => afterOpenModal(subtitle)} onRequestClose={() => closeModal(props.setIsOpen)}style={customStyles}>
+                    <DeleteTextBox>
+                        <DeleteText>Are you sure you want</DeleteText>
+                        <DeleteText>to delete this post?</DeleteText>
+                    </DeleteTextBox>
+                    <ButtonBox>
+                        <Button>No, go back</Button>
+                        <Button>Yes, delete it</Button>
+                    </ButtonBox>
+            </Modal>
+        </>
+    )
+}
--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -2,51 +2,17 @@ import Publish from "./Publish/Publish.js";
 import Post from "./Post/Post.js";
 import Header from "../Header/Header.js";
 import { useState, useEffect } from "react";
-import { Main, TittleBox, Tittle, LoadingBox, Text, DeleteText, DeleteTextBox, Button, ButtonBox } from "./styles.js"
+import { Main, TittleBox, Tittle, LoadingBox, Text } from "./styles.js"
 import { BallTriangle } from "react-loader-spinner";
 import timelineFunctions from "./functions/timelineFunctions.js";
-import Modal from "react-modal";
- 
-const customStyles = {
-    content: {
-        width: '600px',
-        height: '260px',
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)',
-        background: '#333333',
-        borderRadius: '50px'
-    },
-};
-
+import DeleteModal from "./DeleteModal/DeleteModal.js";
+import { openModal } from "./functions/modalFunctions.js";
 export default function Timeline(){
-    
     const [posts, setPosts] = useState([]);
+    const [modalIsOpen, setIsOpen] = useState(false);
     let [error, setError] = useState("");
     let [loadingState, setLoadingState] = useState(false); 
-
-    let subtitle;
-    const [modalIsOpen, setIsOpen] = useState(false);
-  
-    function openModal() {
-      setIsOpen(true);
-    }
-  
-    function afterOpenModal() {
-      subtitle.style.color = '#f00';
-    }
-  
-    function closeModal() {
-      setIsOpen(false);
-    }
     
-
-    Modal.setAppElement('#root');
-
-
     useEffect(() => {
         setTimeout(() => {
             timelineFunctions.getPosts(posts, setPosts, setError, setLoadingState);
@@ -57,20 +23,11 @@ export default function Timeline(){
         <>
             <Header></Header>
             <Main>
-                <Modal isOpen={modalIsOpen} onAfterOpen={afterOpenModal} onRequestClose={closeModal}style={customStyles}>
-                    <DeleteTextBox>
-                        <DeleteText>Are you sure you want</DeleteText>
-                        <DeleteText>to delete this post?</DeleteText>
-                    </DeleteTextBox>
-                    <ButtonBox>
-                        <Button>No, go back</Button>
-                        <Button>Yes, delete it</Button>
-                    </ButtonBox>
-                </Modal>
+                <DeleteModal modalIsOpen={modalIsOpen} setIsOpen={setIsOpen}></DeleteModal>
                 <TittleBox>
                         <Tittle /*onClick={() => {
                             timelineFunctions.getPosts(posts, setPosts, setError, setLoadingState);
-                        }}*/ onClick={openModal}> timeline </Tittle>
+                        }}*/ onClick={() => openModal(setIsOpen)}> timeline </Tittle>
                 </TittleBox>
                 <Publish posts={posts} setPosts={setPosts} setError={setError} setLoadingState={setLoadingState}></Publish>
                 {
